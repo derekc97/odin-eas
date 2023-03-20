64eff6a35523364ed7f6075ad6eb71ff2819efb0
derekc97 <derekchong97@gmail.com>
2023-03-20 18:46:37 -0400

    colors added

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let gridSize = 10;
+let gridSize = 100;
 let easGrid = document.querySelector(".eas-grid");
 let body = document.querySelector("body");
 let brushColor = "black";
+const blueButton = document.querySelector("#blue");
+const redButton = document.querySelector("#red");
+const greenButton = document.querySelector("#green");
+
+blueButton.addEventListener("click", ()=> {
+    brushColor = "blue";
+    paintColor(brushColor);
+})
+
+redButton.addEventListener("click", ()=> {
+    brushColor = "red";
+    paintColor(brushColor);
+})
+
+greenButton.addEventListener("click", ()=> {
+    brushColor = "green";
+    paintColor(brushColor);
+})
 
 function initialGrid (size) {
     let gridAmount = size * size;
@@ -15,11 +33,14 @@ function initialGrid (size) {
     easGrid.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
 }
 
-function paintColor() {
+function paintColor(color) {
     let gridArray = document.querySelectorAll(".grid-square");
     for (let i = 0; i < gridArray.length; i++) {
-        gridArray[i].addEventListener
+        gridArray[i].addEventListener("mouseover", ()=> {
+            gridArray[i].style.backgroundColor = color;
+        })
     }
 }
 
 initialGrid(gridSize);
+paintColor(brushColor);
