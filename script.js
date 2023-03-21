2d53f05403c9fb3154ced9da07b1946b8b175d70
derekc97 <derekchong97@gmail.com>
2023-03-20 23:14:34 -0400

    done aside from rainbow

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let gridSize = 100;
+let gridSize = 16;
 let easGrid = document.querySelector(".eas-grid");
 let body = document.querySelector("body");
 let brushColor = "black";
+const blackButton = document.querySelector("#black");
 const blueButton = document.querySelector("#blue");
 const redButton = document.querySelector("#red");
 const greenButton = document.querySelector("#green");
+const eraseButton = document.querySelector("#erase");
+const resetButton = document.querySelector("#reset");
+const changeButton = document.querySelector("#change-size");
+
+changeButton.addEventListener("click", ()=> {
+    let gridArray = document.querySelectorAll(".grid-square")
+    // let holder = gridSize;
+    let userInput = window.prompt("Input size of grid: ");
+    gridSize = userInput;
+    for (let i = 0; i < gridArray.length; i++) {
+        easGrid.removeChild(gridArray[i]);
+    }
+    initialGrid(gridSize);
+    paintColor(brushColor);
+})
+
+blackButton.addEventListener("click", ()=> {
+    brushColor = "black";
+    paintColor(brushColor);
+})
 
 blueButton.addEventListener("click", ()=> {
     brushColor = "blue";
@@ -19,6 +40,18 @@ redButton.addEventListener("click", ()=> {
 greenButton.addEventListener("click", ()=> {
     brushColor = "green";
     paintColor(brushColor);
+})
+
+eraseButton.addEventListener("click", ()=> {
+    brushColor = "white";
+    paintColor(brushColor);
+})
+
+resetButton.addEventListener("click", ()=> {
+    let gridArray = document.querySelectorAll(".grid-square");
+    for (let i = 0; i < gridArray.length; i++) {
+        gridArray[i].style.backgroundColor = "white";
+    }
 })
 
 function initialGrid (size) {
